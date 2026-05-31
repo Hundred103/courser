636a089f6be241cadb1b8c791f1ad16a4a82d33a
Ku1son <szymonkula2005@gmail.com>
2026-05-31 23:48:38 +0200

    import

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { CanDeactivateFn, Routes } from '@angular/router';
 import { HomePageComponent } from './features/home/home-page.component';
 import { QuizCreatePageComponent } from './features/quiz-create/quiz-create-page.component';
+import { QuizImportPageComponent } from './features/quiz-import/quiz-import-page.component';
 import { QuizPlayPageComponent } from './features/quiz-play/quiz-play-page.component';
 
 const canDeactivateQuiz: CanDeactivateFn<QuizPlayPageComponent> = (component) => component.canDeactivate();
@@ -8,6 +9,7 @@ const canDeactivateQuizCreate: CanDeactivateFn<QuizCreatePageComponent> = (compo
 
 export const APP_ROUTES: Routes = [
   { path: '', component: HomePageComponent },
+  { path: 'quizzes/import', component: QuizImportPageComponent },
   { path: 'quizzes/new', component: QuizCreatePageComponent, canDeactivate: [canDeactivateQuizCreate] },
   { path: 'quizzes/:id/edit', component: QuizCreatePageComponent, canDeactivate: [canDeactivateQuizCreate] },
   { path: 'quizzes/:id/play', component: QuizPlayPageComponent, canDeactivate: [canDeactivateQuiz] },
