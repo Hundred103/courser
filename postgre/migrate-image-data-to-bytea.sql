14b70959f4392822d4990460b7a255dc9245bbf1
Hundred103 <mateusz1mich@gmail.com>
2026-06-29 19:30:54 +0200

    Fix: Returned the old Szymon account for old quiz access. Cleanup

--- postgre/migrate-image-data-to-bytea.sql
+++
@@ -1,9 +0,0 @@
--- Migrate image_data from PostgreSQL OID (JPA @Lob) to BYTEA
-ALTER TABLE questions ADD COLUMN IF NOT EXISTS image_data_bytea bytea;
-
-UPDATE questions
-SET image_data_bytea = lo_get(image_data)
-WHERE image_data IS NOT NULL AND image_data_bytea IS NULL;
-
-ALTER TABLE questions DROP COLUMN IF EXISTS image_data;
-ALTER TABLE questions RENAME COLUMN image_data_bytea TO image_data;
